f4c520fe02de6b89eb5092abd723bbbd78cd14de
Sazzad-Hossain-Sobuj <sazzad15-2596@diu.edu.bd>
2023-11-03 22:44:42 +0600

    module 11 liveTest

--- lib/item_list.dart
+++
@@ -1,8 +0,0 @@
-class ListItem {
-  final String title;
-  final String subtitle;
-
-  ListItem({
-    required this.title,
-    required this.subtitle});
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'home_page.dart';
-void main() {
-  runApp(ToDoApp());
+
+void main(){
+  runApp(MyApp());
 }
 
-class ToDoApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.pink),
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: HomeScreen(),
     );
   }
 }
