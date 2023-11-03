dcd71dc8d8082f646ad5d77c259528ff891641db
Sazzad-Hossain-Sobuj <sazzad15-2596@diu.edu.bd>
2023-11-03 23:32:02 +0600

    module 11 liveTest

---
+++ lib/item_list.dart
@@ -0,0 +1,10 @@
+class WeatherCard {
+  final String city;
+  final int temperature;
+  final String condition;
+  final int humidity;
+  final double windSpeed;
+
+  WeatherCard(this.city, this.temperature, this.condition, this.humidity,
+      this.windSpeed);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,102 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
+import 'item_list.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final jsonString = '''
+  [
+    {
+      "city": "New York",
+      "temperature": 30,
+      "condition": "clear",
+      "humidity": 40,
+      "windSpeed": 4
+    },
+    {
+      "city": "Los Angeles",
+      "temperature": 25,
+      "condition": "Sunny",
+      "humidity": 30,
+      "windSpeed": 4
+    },
+    {
+      "city": "London",
+      "temperature": 15,
+      "condition": "Partly Cloudy",
+      "humidity": 50,
+      "windSpeed": 5
+    },
+    {
+      "city": "Tokyo",
+      "temperature": 28,
+      "condition": "Rainy",
+      "humidity": 40,
+      "windSpeed": 8
+    },
+    {
+      "city": "Sydney",
+      "temperature": 22,
+      "condition": "Cloudy",
+      "humidity": 55,
+      "windSpeed": 7
+    }
+  ]
+  ''';
+
+  List<WeatherCard> weatherData = [];
+
+  MyApp() {
+    final data = jsonDecode(jsonString);
+    for (var item in data) {
+      final card = WeatherCard(
+        item['city'],
+        item['temperature'],
+        item['condition'],
+        item['humidity'],
+        item['windSpeed'],
+      );
+      weatherData.add(card);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Weather Info App'),
+        ),
+        body: ListView.builder(
+          itemCount: weatherData.length,
+          itemBuilder: (context, index) {
+            return Card(
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ListTile(
+                title: Text('City: ${weatherData[index].city}'),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Temperature: ${weatherData[index].temperature}°C'),
+                    Text('Condition: ${weatherData[index].condition}'),
+                    Text('Humidity: ${weatherData[index].humidity}%'),
+                    Text('Wind Speed: ${weatherData[index].windSpeed} m/s'),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
